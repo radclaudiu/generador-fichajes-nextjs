0b9b8334086f9dfd59f9c5f8db263834f09d0e0b
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-20 20:24:33 +0200

    Add login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,34 @@
+import { login } from "../lib/auth";
+import { redirect } from "next/navigation";
+
+export default function LoginPage() {
+  return (
+    <section className="flex flex-col items-center justify-center h-screen flex-grow">
+      <h1>Inicia sesion para continuar</h1>
+      <form
+        action={async (formData) => {
+          'use server';
+          await login(formData);
+          redirect("/companies");
+        }}
+        className="flex flex-col mt-4 space-y-4">
+        <input
+          type="text"
+          name="username"
+          placeholder="Nombre de usuario"
+          className="border p-2 rounded"
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          className="border p-2 rounded"
+        />
+        <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+          Iniciar sesión
+        </button>
+      </form>
+    </section>
+
+  );
+}
