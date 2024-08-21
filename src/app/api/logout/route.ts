c27cfc79eca99f8428d9f722aa78f05bc1daed11
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-21 21:12:40 +0200

    Add logout function

---
+++ src/app/api/logout/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+import { logout } from '../../lib/auth'; // Ajusta la ruta según tu estructura
+
+
+export async function GET(req: NextRequest) {
+  try {
+      // Elimina la cookie de sesión
+      await logout();
+      // Redirige al usuario a la página de inicio de sesión
+      const session = req.cookies.get('session')?.value;
+      const baseUrl = req.url.split('?')[0];
+      const domain = req.headers.get('host');
+      return NextResponse.redirect(`http://${domain}/login`);
+  } catch (error) {
+      console.error('Failed to log out:', error);
+      return NextResponse.json({ error: 'Failed to log out' }, { status: 500 });
+  }
+}
--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -2,13 +2,19 @@ import Link from "next/link";
 import { Company } from "../lib/definitions";
 import { deleteCompany, fetchCompanies } from "../lib/data";
 import { MainTitle } from "../ui/titles";
+import { logout } from "../lib/auth";
 
 export default async function CompaniesPage() {
     const companies: Company[] = await fetchCompanies();
 
     return (
         <section className="flex flex-col items-center justify-center h-screen flex-grow">
-            <button className='bg-red-500 hover:bg-red-600 text-white m-5 p-2 rounded' >Cerrar sesión</button>
+            <a
+                href={'/api/logout'}	
+                className='bg-red-500 hover:bg-red-600 text-white m-5 p-2 rounded'
+            >
+                Cerrar sesión
+            </a>
             <MainTitle>Empresas</MainTitle>
             <div className="grid auto-rows-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 rows justify-evenly gap-4 max-h-[60vh] max-w-[60vw] w-full overflow-y-scroll mt-auto flex-grow">
                 {companies.map((company) => (
