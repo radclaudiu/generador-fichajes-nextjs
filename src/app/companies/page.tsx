6a89b81e68325f80b41edc84a78799359b5804ee
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-21 15:30:25 +0200

    Fetch companies instead of using placeholder data

--- src/app/companies/page.tsx
+++ src/app/companies/page.tsx
@@ -1,42 +1,10 @@
 import Link from "next/link";
 import { Company } from "../lib/definitions";
+import { fetchCompanies } from "../lib/data";
 
-export default function CompaniesPage() {
-    const companies: Company[] = [
-        {
-            id: 1,
-            name: "Company 1",
-            cif: "12345678A",
-            address: "Calle Falsa 123",
-            cp: "12345",
-            city: "Springfield"
-        },
-        {
-            id: 2,
-            name: "Company 2",
-            cif: "87654321B",
-            address: "Calle Falsa 456",
-            cp: "54321",
-            city: "Shelbyville"
-        },
-        {
-            id: 3,
-            name: "Company 3",
-            cif: "11111111C",
-            address: "Calle Falsa 789",
-            cp: "67890",
-            city: "Ogdenville"
-        },
-        {
-            id: 4,
-            name: "Company 4",
-            cif: "22222222D",
-            address: "Calle Falsa 1011",
-            cp: "11213",
-            city: "North Haverbrook"
-        }
-    ];
-    // Create page with a list of cards to show companies with 4 buttons to edit, delete, view and create a new company
+export default async function CompaniesPage() {
+    const companies: Company[] = await fetchCompanies();
+    
     return (
         <section className="flex flex-col items-center justify-center h-screen flex-grow">
             <h1 className="mb-3">Empresas</h1>
