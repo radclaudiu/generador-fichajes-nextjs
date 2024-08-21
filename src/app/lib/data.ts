ec0a7496a76ce0f72571564321ee47c152e2b256
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-21 15:17:29 +0200

    Add fetch to retrieve all companies

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,17 +1,27 @@
 import { sql } from '@vercel/postgres';
 import {
-  Employee
+  Employee,
+  Company
 } from './definitions';
 
-export async function fetchEmployees(companyName: string) : Promise<Employee[]> {
-    try {
-  
-      const data = await sql<Employee>`SELECT * FROM employees`;
-  
-      return data.rows;
-    } catch (error) {
-      console.log('Database Error:', error);
-      console.log(error);
-      throw new Error('Failed to fetch employees data.');
-    }
+export async function fetchEmployees(companyName: string): Promise<Employee[]> {
+  try {
+    const data = await sql<Employee>`SELECT * FROM employees`;
+    return data.rows;
+  } catch (error) {
+    console.log('Database Error:', error);
+    console.log(error);
+    throw new Error('Failed to fetch employees data.');
   }
+}
+
+export async function fetchCompanies(): Promise<Company[]> {
+  try {
+    const data = await sql<Company>`SELECT * FROM companies`;
+    return data.rows;
+  } catch (error) {
+    console.log('Database Error:', error);
+    console.log(error);
+    throw new Error('Failed to fetch companies data.');
+  }
+}
