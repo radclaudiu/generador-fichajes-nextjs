4ded30efa35759cb50664ac0540e61ef38b0f188
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-22 18:26:15 +0200

    Remove unused imports and redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./app/lib/auth";
 import { JWTClaimValidationFailed } from 'jose/errors';
-import { log } from "console";
-import { Odor_Mean_Chey } from "next/font/google";
+
+
 
 export default async function middleware(req: NextRequest) {
     const session = req.cookies.get('session')?.value;
     const baseUrl = req.url.split('?')[0];
     const domain = req.headers.get('host');
     
-    if (!req.url.includes('login') && !session) {
-        return NextResponse.redirect(`http://${domain}/login`);
+    if (req.url.includes('api') && !req.url.includes('login') && !session) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    if (!req.url.includes('login') && !req.url.includes('api') && !session) {
+        return NextResponse.redirect(`${process.env.BASE_URL}/login`);
+    }
+   
+    if (req.url.includes('login') && !req.url.includes('api') && session) {
+        return NextResponse.redirect(`${process.env.BASE_URL}/companies`);
     }
     try {
         return await updateSession(req);
