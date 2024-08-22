40095dadc2618bff99dfaacc6166d87b44c156d5
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-22 18:55:39 +0200

    Add update employee endpoint

--- src/app/api/v0/employees/[employeeId]/route.ts
+++ src/app/api/v0/employees/[employeeId]/route.ts
@@ -1,1 +1,15 @@
+import { NextResponse } from 'next/server';
+import { updateEmployee } from '@/app/lib/data/employees';
 
+
+export async function PUT(request: Request, context: any): Promise<NextResponse> {
+    const employeeId = context.params;
+    const employeeData = await request.json();
+    try {
+        const employee = await updateEmployee(employeeId, employeeData);
+        return NextResponse.json(employee);
+    } catch (error) {
+        console.log('Database Error:', error);
+        return NextResponse.json({ error: 'Failed to create employee.' }, { status: 500 });
+    }
+}
