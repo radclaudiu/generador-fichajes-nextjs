80224e77afcbfc3a3b32f1e5fc6e1408dd1608fa
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-28 02:19:34 +0200

    Update data definitions and placeholder-data

--- src/app/lib/definitions.ts
+++ src/app/lib/definitions.ts
@@ -3,7 +3,30 @@ export type Employee = {
     name: string;
     dni: string;
     nss: string;
+    monday_in: string;
+    monday_out: string;
+    tuesday_in: string;
+    tuesday_out: string;
+    wednesday_in: string;
+    wednesday_out: string;
+    thursday_in: string;
+    thursday_out: string;
+    friday_in: string;
+    friday_out: string;
+    saturday_in: string;
+    saturday_out: string;
+    sunday_in: string;
+    sunday_out: string;
+    vacations: Vacation[];
+    checks: Check[];
     companyId: number;
+}
+
+export type Vacation = {
+    id: number;
+    start_date: string;
+    end_date: string;
+    employee_id: number;
 }
 
 export type Company = {
@@ -13,4 +36,17 @@ export type Company = {
     address: string;
     cp: string;
     city: string;
+}
+
+export type Check = {
+    date: Date;
+    start_time: Date;
+    end_time: Date;
+    employeeId: number;
+}
+
+export type Schedule = {
+    start: string;
+    end: string;
+    employeeId: number;
 }
--- src/app/lib/placeholder-data.ts
+++ src/app/lib/placeholder-data.ts
@@ -1,39 +1,82 @@
 import { Employee, Company } from './definitions';
 
-const employees: Employee[] = [
+const employees: any[] = [
     {
-        id: 1,
         name: "Employee 1",
         dni: "12345678A",
         nss: "123456789012",
         companyId: 1
     },
     {
-        id: 2,
         name: "Employee 2",
         dni: "87654321B",
         nss: "987654321012",
         companyId: 2
     },
     {
-        id: 3,
         name: "Employee 3",
         dni: "11111111C",
         nss: "111111111012",
         companyId: 3
     },
     {
-        id: 4,
         name: "Employee 4",
         dni: "22222222D",
         nss: "222222222012",
         companyId: 4
+    },
+    {
+        name: "Employee 5",
+        dni: "33333333E",
+        nss: "333333333012",
+        companyId: 1
+    },
+    {
+        name: "Employee 6",
+        dni: "44444444F",
+        nss: "444444444012",
+        companyId: 2
+    },
+    {
+        name: "Employee 7",
+        dni: "55555555G",
+        nss: "555555555012",
+        companyId: 3
+    },
+    {
+        name: "Employee 8",
+        dni: "66666666H",
+        nss: "666666666012",
+        companyId: 4
+    },
+    {
+        name: "Employee 9",
+        dni: "77777777I",
+        nss: "777777777012",
+        companyId: 1
+    },
+    {
+        name: "Employee 10",
+        dni: "88888888J",
+        nss: "888888888012",
+        companyId: 2
+    },
+    {
+        name: "Employee 11",
+        dni: "99999999K",
+        nss: "999999999012",
+        companyId: 3
+    },
+    {
+        name: "Employee 12",
+        dni: "00000000L",
+        nss: "000000000012",
+        companyId: 4
     }
 ];
 
-const companies: Company[] = [
+const companies: any[] = [
     {
-        id: 1,
         name: "Company 1",
         cif: "12345678A",
         address: "Calle Falsa 123",
@@ -41,7 +84,6 @@ const companies: Company[] = [
         city: "Springfield"
     },
     {
-        id: 2,
         name: "Company 2",
         cif: "87654321B",
         address: "Calle Falsa 456",
@@ -49,7 +91,6 @@ const companies: Company[] = [
         city: "Shelbyville"
     },
     {
-        id: 3,
         name: "Company 3",
         cif: "11111111C",
         address: "Calle Falsa 789",
@@ -57,12 +98,18 @@ const companies: Company[] = [
         city: "Ogdenville"
     },
     {
-        id: 4,
         name: "Company 4",
         cif: "22222222D",
         address: "Calle Falsa 1011",
         cp: "11213",
         city: "North Haverbrook"
+    }, 
+    {
+        name: "Company 5",
+        cif: "33333333E",
+        address: "Calle Falsa 1213",
+        cp: "14151",
+        city: "Capital City"
     }
 ];
 
