04a157d582f7d5497e8adac11703d177dd536a37
Adolfo Montes Luque <montesluquead@gmail.com>
2024-08-28 02:20:43 +0200

    Create and update custom buttons

--- src/app/ui/buttons.tsx
+++ src/app/ui/buttons.tsx
@@ -2,20 +2,31 @@
 
 import { redirect } from "next/dist/client/components/navigation";
 
+import { useRouter } from 'next/navigation';
 
+export function LogoutButton({ children }: { children: string }) {
+    "use client";
+    const router = useRouter();
 
-export function LogoutButton({ children } : {children: string}) {
-    const handleLogout = () => {
-        fetch('/api/v0/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({}),
-        }).then(() => {
-            // Redirect to login page in a client-side navigation
-            redirect('/login');
-        });
+    const handleLogout = async () => {
+        try {
+            const response = await fetch('/api/v0/logout', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({}),
+            });
+
+            if (response.ok) {
+                // Redirect to login page
+                router.push('/login');
+            } else {
+                console.error('Logout failed');
+            }
+        } catch (error) {
+            console.error('An error occurred during logout:', error);
+        }
     };
 
     return (
@@ -24,3 +35,45 @@ export function LogoutButton({ children } : {children: string}) {
         </button>
     );
 };
+
+
+export function CreateButton({ children } : {children: string}) {
+    return (
+        <button className='bg-blue-500 hover:bg-blue-600 text-white m-5 p-2 rounded'>
+            {children}
+        </button>
+    );
+};
+
+export function EditButton({ children } : {children: string}) {
+    return (
+        <button className='bg-blue-500 hover:bg-blue-600 text-white p-2 rounded'>
+            {children}
+        </button>
+    );
+};
+
+export function DeleteButton({ children } : {children: string}) {
+    return (
+        <button className='bg-red-500 hover:bg-red-600 text-white p-2 rounded'>
+            {children}
+        </button>
+    );
+};
+
+export function SaveButton({ children } : {children: string}) {
+    return (
+        <button className='bg-green-500 hover:bg-green-600 text-white p-2 rounded'>
+            {children}
+        </button>
+    );
+};
+
+export function CancelButton({ children, ...props } : {children: string}) {
+    return (
+        <button {...props} className='bg-red-500 hover:bg-red-600 text-white p-2 rounded'>
+            {children}
+        </button>
+    );
+};
+
